5bbb1b3852eb52d8cadb69c20a82bb585559c2f7
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-07-25 14:14:22 +0300

    Add valuta's shell representation

---
+++ src/ValCurs.java
@@ -0,0 +1,46 @@
+import jakarta.xml.bind.annotation.*;
+
+import java.util.ArrayList;
+
+/**
+ * Класс описывающий оболочку для списка всех полученных по запросу валют
+ */
+@XmlRootElement(name = "ValCurs")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ValCurs {
+    @XmlAttribute(name = "Date")
+    private String date;
+
+    @XmlAttribute(name = "name")
+    private String name;
+    @XmlElement(name = "Valute")
+    private ArrayList<Valuta> valutes;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Valuta> getValutes() {
+        return valutes;
+    }
+
+    public Valuta getValuta(String codeIso) {
+        for (Valuta valuta : valutes) {
+            if (valuta.getId().equals(codeIso))
+                return valuta;
+        }
+        return null;
+    }
+}
