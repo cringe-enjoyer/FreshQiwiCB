a8ff2fcb943f82553bf1e37c7c1c5ed56ff99c5c
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-07-25 15:23:25 +0300

    Reformat to args

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,20 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;
 
 public class Main {
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     public static void main(String[] args) {
-        boolean isWork = true;
-        Scanner scanner = new Scanner(System.in);
-        while (isWork) {
-            System.out.println("Введите ISO код валюты");
-            String codeISO = scanner.nextLine();
-            System.out.println("Введите дату в формате YYYY-MM-DD");
+        //args = USD 2022-10-08
+        //boolean isWork = true;
+        //Scanner scanner = new Scanner(System.in);
+        //while (isWork) {
+            //System.out.println("Введите ISO код валюты");
+            //scanner.nextLine();
+            //System.out.println("Введите дату в формате YYYY-MM-DD");
             try {
-                Date date = convertToDate(scanner.nextLine());
+                String codeISO = args[0];
+                Date date = convertToDate(args[1]);//scanner.nextLine());
                 ValCurs valuta = WebUtil.sendRequest(date);
                 System.out.println(valuta.getValuta(codeISO));
             } catch (ParseException e) {
@@ -26,11 +27,15 @@ public class Main {
                 System.err.println(e);
                 System.out.println("Ошибка запроса");
             }
-            System.out.println("Завершить работу приложения?\nДа/Нет");
-            String answer = scanner.nextLine();
-            isWork = answer.equals("Нет") || answer.equals("нет");
-        }
-        scanner.close();
+            catch (Exception e) {
+                System.err.println(e);
+                System.out.println("Неверные параметры");
+            }
+            //System.out.println("Завершить работу утилиты?\nДа/Нет");
+            //String answer = scanner.nextLine();
+            //isWork = answer.equals("Нет") || answer.equals("нет");
+        //}
+        //scanner.close();
     }
 
     private static Date convertToDate(String date) throws ParseException {
